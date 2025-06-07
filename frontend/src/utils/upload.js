132f777069fe07ec75f0a4c3df3ff6c1f5918d76
Asad Ishaq <167089251+axadishaq@users.noreply.github.com>
2025-06-07 18:22:05 +0500

    Update upload.js

--- frontend/src/utils/upload.js
+++ frontend/src/utils/upload.js
@@ -1,17 +1,18 @@
 import axios from "axios";
+const CLOUDINARY_URL = import.meta.env.VITE_CLOUDINARY_URL;
+
 const upload = async (file) => {
    const data = new FormData();
    data.append("file", file);
    data.append("upload_preset", "uraaan");
    try {
       const res = await axios.post(
-         "https://api.cloudinary.com/v1_1/axad/image/upload",
+         CLOUDINARY_URL,
          data
       );
-
       return res.data.url;
    } catch (err) {
-      console.log("Image upload error", err, data.url);
+      console.log("Image upload error", err);
    }
 };
 export default upload;
